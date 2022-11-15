c81e90972f3ca525d68c29a706ffa5bd97b44c58
Shadow4k19 <114390524+Shadow4k19@users.noreply.github.com>
2022-11-15 23:48:08 +0700

    Clean Not Code It My head SAD

--- src/component/Q&A.js
+++ src/component/Q&A.js
@@ -4,6 +4,7 @@ export default function QandA(){
     return(
         <div><h1 style={{color:'black',paddingLeft:'780px',paddingTop:'75px'}}>Q&A</h1>
         <h3 style={{color:'black',paddingLeft:'545px',paddingTop:'50px'}}>Go to function what ever you want and use it.</h3>
+        <img src="image/Ayaka_TEST.jpg" alt = "Ayaka" width="350px" height="300px" style={{paddingLeft:"650px",paddingTop:"70px"}} />   
         </div>
     )
 }
